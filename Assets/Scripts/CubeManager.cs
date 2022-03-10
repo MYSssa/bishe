aea4ae09f389214e768a603aa1f0452d27439a77
MYSssa <1120094008@qq.com>
2022-03-10 23:47:59 +0800

    添加光剑特效之前，在store上下载了一个，但还没用，不想看代码

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,34 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+
+    public GameObject cubePb;
+    public Transform[] originTrans;
+    public Transform[] targetTrans;
+    void Start()
+    {
+        InvokeRepeating("creatCube", 0, 2f);
+    }
+    void Update()
+    {
+
+    }
+    void creatCube()
+    {
+        int random = Random.Range(0, 2);
+        GameObject obj = Instantiate(cubePb, originTrans[random].position, cubePb.transform.rotation);
+        //obj.transform.DOMove(targetTrans[random].position, 5).OnComplete(delegate () {
+        //    //播放完动画后销毁
+        //    Destroy(obj);
+        //});
+        obj.transform.DOMove(targetTrans[random].position, 5).OnComplete(() =>
+        {
+            //播放完动画后销毁
+            Destroy(obj);
+        });
+    }
+}
---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,50 @@
+﻿using EzySlice;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slicer : MonoBehaviour
+{
+    public Material sliceSurface;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //省略
+        Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale / 2, transform.rotation, LayerMask.GetMask("Slice"));
+        if (colliders.Length > 0)
+        {
+            foreach (var collider in colliders)
+            {
+                Destroy(collider.gameObject);
+                //位置与法线获得切割面
+                SlicedHull hull = collider.gameObject.Slice(transform.position, transform.up);
+                if (hull != null)
+                {
+                    GameObject upper = hull.CreateLowerHull(collider.gameObject, sliceSurface);
+                    GameObject lower = hull.CreateUpperHull(collider.gameObject, sliceSurface);
+                    GameObject[] objs = new GameObject[] { upper, lower };
+
+                    foreach (var obj in objs)
+                    {
+                        //添加刚体并设为凸多面体
+                        obj.gameObject.AddComponent<Rigidbody>();
+                        obj.gameObject.AddComponent<MeshCollider>().convex = true;
+                    }
+                    //施加力
+                    objs[0].gameObject.GetComponent<Rigidbody>().AddForce(transform.up * 100);
+                    objs[1].gameObject.GetComponent<Rigidbody>().AddForce(-transform.up * 100);
+                }
+            }
+        }
+
+
+
+    }
+}
